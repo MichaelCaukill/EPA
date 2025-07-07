9e581a13dd7b112e9fb561e6105baad4fc45d868
Michael <michaelcaukill@hotmail.com>
2025-07-07 10:53:33 +0100

    Changed public ip filepath and comment

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -67,7 +67,7 @@ resource "aws_key_pair" "deployer" {
   public_key = tls_private_key.deployer.public_key_openssh
 }
 
-# 4. (Optional) Save the private key locally for SSH access
+# 4. Save the private key locally for SSH access
 resource "local_file" "private_key" {
   content         = tls_private_key.deployer.private_key_pem
   filename        = "${path.module}/ephemeral_key.pem"
